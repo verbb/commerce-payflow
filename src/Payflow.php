b3a3076977f9fde4a12c07e2a5d1627f7ecaec4a
Josh Crawford <joshua@sgroup.com.au>
2018-11-23 14:31:53 +1100

    Add hook to add currency when paying or storing cards

--- src/Payflow.php
+++ src/Payflow.php
@@ -8,6 +8,8 @@ use craft\base\Plugin;
 use craft\events\RegisterComponentTypesEvent;
 
 use craft\commerce\services\Gateways;
+use craft\commerce\omnipay\events\SendPaymentRequestEvent;
+use craft\commerce\omnipay\base\Gateway as Gateway;
 
 use yii\base\Event;
 
@@ -18,6 +20,7 @@ class Payflow extends Plugin
 
     public $schemaVersion = '1.0.0';
 
+    
     // Public Methods
     // =========================================================================
 
@@ -27,6 +30,13 @@ class Payflow extends Plugin
 
         Event::on(Gateways::class, Gateways::EVENT_REGISTER_GATEWAY_TYPES, function(RegisterComponentTypesEvent $event) {
             $event->types[] = PayflowGateway::class;
+        });
+
+        // When using a saved card, or creating one, we must add the currency to the request - will receive 
+        // `Invalid or unsupported currency code` otherwise
+        Event::on(Gateway::class, Gateway::EVENT_BEFORE_SEND_PAYMENT_REQUEST, function(SendPaymentRequestEvent $e) {
+            $e->modifiedRequestData = $e->requestData;
+            $e->modifiedRequestData['CURRENCY'] = 'AUD';
         });
     }
 }
